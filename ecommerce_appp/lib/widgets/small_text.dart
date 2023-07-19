abc8d32dea9cc02f912a5b802b7d22e030187bc3
White_album <113410310+khamlarPMC@users.noreply.github.com>
2023-07-19 09:47:20 +0700

    Add file
    add file small_text.dart

---
+++ ecommerce_appp/lib/widgets/small_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SmallText extends StatelessWidget {
+  final color;
+  final String text;
+  final double size;
+  final double height;
+  const SmallText({
+    Key? key,
+    this.color = const Color(0xFF89dad0),
+    required this.text,
+    this.size = 12,
+    this.height = 1.2,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+        height: height,
+      ),
+    );
+  }
+}
